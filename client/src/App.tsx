7f377b09da93e800116765ca873d9a2ccc3a77b2
krish-anu <krishnaanu200302@gmail.com>
2025-07-24 11:05:50 +0530

    installation and run the frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import TodoApp from "./components/TodoApp.tsx";
 import "./index.css"; // Make sure Tailwind CSS is imported
 function App() {
